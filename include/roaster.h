e5005484e8fee8d42f19f1989d67fb08988ab7df
Alexei Chetroi <Alexei.Chetroi@outlook.com>
2024-05-25 09:45:54 -0400

    Update pin definitions

--- include/roaster.h
+++ include/roaster.h
@@ -15,4 +15,18 @@
 #define TEMPERATURE_ROASTER(x) x.chanTemp[TEMPERATURE_CHANNEL_ROASTER]
 #define TEMPERATURE_TC(x)      x.chanTemp[TEMPERATURE_CHANNEL_THERMOCOUPLE]
 
+// PIN Definitions
+#define PIN_LED     PA3
+#define PIN_EXHAUST PA8
+#define PIN_DRUM    PA15
+#define PIN_HEAT    PB9
+#define PIN_NTC     PC5
+#define PIN_COOL    PC8
+
+#define SPI_BTCS    PA4
+#define SCK         PA5
+#define SPI_SO      PA6
+#define I2C_SCL     PB6
+#define I2C_SDA     PB7
+
 #endif  // __ROASTER_H
