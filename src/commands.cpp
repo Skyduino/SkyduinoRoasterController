58a0adc97d570bf091c6da8e1e7906d238cd4ab1
Alexei Chetroi <Alexei.Chetroi@outlook.com>
2024-05-27 12:39:47 -0400

    Style: sort cmnd instances.

--- src/commands.cpp
+++ src/commands.cpp
@@ -19,31 +19,31 @@
  */
 extern t_State state;
 
-cmndRead    cmnd_handler_read;
 cmndChan    cmnd_handler_chan;
-cmndVersion cmnd_handler_version;
-cmndOT1     cmnd_handler_ot1 = cmndOT1(&(state.commanded.heat));
-cmndOT2     cmnd_handler_ot2 = cmndOT2(&(state.commanded.vent));
-cmndLED     cmnd_handler_led = cmndLED();
 cmndCool    cmnd_handler_cool= cmndCool(&(state.commanded.cool));
 cmndDrum    cmnd_handler_drum= cmndDrum(&(state.commanded.drum));
 cmndDFU     cmnd_handler_dfu;
+cmndLED     cmnd_handler_led = cmndLED();
+cmndOT1     cmnd_handler_ot1 = cmndOT1(&(state.commanded.heat));
+cmndOT2     cmnd_handler_ot2 = cmndOT2(&(state.commanded.vent));
+cmndRead    cmnd_handler_read;
 cmndUnit    cmnd_handler_unit= cmndUnit(&state);
+cmndVersion cmnd_handler_version;
 
 static CmndInterp ci( DELIM ); // command interpreter object
 
 void setupCommandHandlers(void) {
     Command* commands[] = {
-        &cmnd_handler_chan,
-        &cmnd_handler_read,
+        &cmnd_handler_led,
         &cmnd_handler_version,
-        &cmnd_handler_ot1,
-        &cmnd_handler_ot2,
-        &cmnd_handler_cool,
-        &cmnd_handler_drum,
         &cmnd_handler_dfu,
         &cmnd_handler_unit,
-        &cmnd_handler_led
+        &cmnd_handler_chan,
+        &cmnd_handler_cool,
+        &cmnd_handler_drum,
+        &cmnd_handler_ot1,
+        &cmnd_handler_ot2,
+        &cmnd_handler_read
     };
 
     uint8_t count = sizeof(commands) / sizeof(commands[0]);
